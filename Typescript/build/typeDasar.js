df51545ab5f830ddb25624f656eec1eecb625419
Haidar Oktafiansyah <54706023+haidaroktafiansyah@users.noreply.github.com>
2021-01-07 10:10:21 +0700

    add ts

---
+++ Typescript/build/typeDasar.js
@@ -0,0 +1,9 @@
+"use strict";
+//spesific variable
+var nama = "Nusendra";
+var umur = 25;
+//any
+var heroues = "indonesia";
+heroues = 20;
+heroues = [];
+heroues = {};
---
+++ Typescript/src/typeDasar.ts
@@ -0,0 +1,11 @@
+//spesific variable
+
+let nama:String  = "Nusendra";
+
+let umur = 25;
+
+//any
+let heroues : any = "indonesia";
+heroues = 20;
+heroues = [];
+heroues = {}; 
